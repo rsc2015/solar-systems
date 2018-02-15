b5fcb0c77c65eed807ad41c9a09918aff90c0cd5
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-02-15 15:45:08 -0600

    100% complete

---
+++ array-methods.js
@@ -0,0 +1,50 @@
+console.log("solar.js");
+const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+
+/*
+    Use the forEach method to add the name of each planet
+    to a section element in your HTML with an id of "planets".
+    Use string templates to construct the DOM elements.
+*/
+const planetEl = document.getElementById("planets");
+
+planets.forEach(function(planet) {
+    planetEl.innerHTML += `<h2>
+                         ${planet} 
+                          </h2>`
+       //console.log(planets);
+});
+
+/*
+    Use the map method to create a new array where the 
+    first letter of each planet is capitalized. Use the
+    `toUpperCase()` method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+*/
+let capitalPlanets = planets.map(function(planet){
+        return planet.charAt(0).toUpperCase()+ planet.slice(1);
+}); 
+console.log("capitalPlanets",capitalPlanets);
+
+/*  Use the filter method to create a new array that
+    contains planets with the letter 'e'. Use the `includes()`
+    method on strings.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+*/
+let ePlanets = [];
+planets.forEach(function(planet) {
+    if (planet.includes("e")) {
+        ePlanets.push(planet);
+    }
+});
+console.log("ePlanets:", ePlanets);
+
+
+// Use the reduce method to create a sentence from the words in the following array
+const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
+
+let sentence = words.reduce(function(prev, curr){
+    return prev + " " + curr ;
+}); 
+console.log("sentence:", sentence);
